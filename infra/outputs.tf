4dd07882a84b545047f9fc13b6f5e4cf05a624db
Victor Efedi <vefedi@earnest.com>
2025-11-20 20:46:31 +0100

    Beginning

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -46,6 +46,6 @@ output "eks_cluster_security_group_id" {
 }
 
 output "eks_node_security_group_id" {
-  description = "Security group ID for EKS nodes"
-  value       = aws_security_group.eks_nodes.id
+  description = "Security group ID for Fincra hello-world pods"
+  value       = aws_security_group.hello_fincra.id  
 }
